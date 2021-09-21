2f7fbffe2ab1cc0e5492ffb75f555bccb0f1c19a
Kunjan Mirgal <55536136+2202-kunjan@users.noreply.github.com>
2021-09-21 05:34:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Relief Fund</title>
+  </head>
+
+  <body>
+    <form  action="payscript.php" method="post"> 
+    <div class="container1">
+      <h1 class="title">KOLHAPUR <span style="color: red;">FLOOD</span> RELIEF</h1>
+      <img class="image" src="i1.jpg" alt="" />
+      <br />
+      <h2>#Kolhapur Needs your Support…………… <span style="color: white;">Donate</span> to Maharashtra</h2>
+      <div class="container2">
+        <p>
+          Support emergency kit for one family: Food grains, warm clothes, bed sheets, toiletry items, cooking utensils, biscuits, medicines, solar lamps, torches, candles etc.
+        </p>
+      <input type="submit"  value="DONATE" class="btn">
+      </div>
+    </div>
+  
+    <div class="container3">
+      <h3>Your support will help us to feed 25 flood affected victims</h3>
+      <p><span style="color:black">
+        “A total of 69,682 families were affected by the recent floods in the district. The highest number of flood-affected people are in the Karveer and Shirol regions. A total of Rs 17.42 crore ex gratia funds have been received for Kolhapur district"
+    </span></p>
+    </div>
+   
+    <div class="footer">
+      <h4>Kolhapur Flood Relief</h4>
+      <p class="p-middle">
+        © Copyright 2021 All rights reserved.
+      </p>
+    </div>
+    </form>
+    <script src="./app.js"></script>
+    
+  </body>
+</html>
